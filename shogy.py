6ae8365e103708fc9a7eb6bc6aa5240ff77831a6
Javier Valdivieso <javier.valdivieso@possumus.tech>
2019-11-12 22:17:29 -0300

    Se corrige estructura de tablero generando una matriz y se crea primer movimiento de peon

--- shogy.py
+++ shogy.py
@@ -1,94 +1,105 @@
+import numpy
+
+
 class Tablero:
 
     def __init__(self):
 
         self.juego = 'iniciado'
 
+        self.columna = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
+        self.fila = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
+
         self.t = [
-            [Lancero('v', 'n', 'r', 'L', (0, 1)), Caballo('v', 'n', 'r', 'N', (0, 4)),
-             GeneralPlata('v', 'n', 'r', 'S', (0, 4)),
-             GeneralOro('v', 'n', 'r', 'G', (0, 4)), Rey('v', 'n', 'r', 'K', (0, 5), False),
-             GeneralOro('v', 'n', 'r', 'G', (0, 6)),
-             GeneralPlata('v', 'n', 'r', 'S', (0, 7)), Caballo('v', 'n', 'r', 'N', (0, 8)),
-             Lancero('v', 'n', 'r', 'L', (0, 9))],
+            [Lancero('v', 'no', 'negro', 'L'), Caballo('v', 'no', 'negro', 'N'), GeneralPlata('v', 'no', 'negro', 'S'),
+             GeneralOro('v', 'negro', 'G'), Rey('v', 'negro', 'K', False), GeneralOro('v', 'negro', 'G'),
+             GeneralPlata('v', 'no', 'negro', 'S'), Caballo('v', 'no', 'negro', 'N'), Lancero('v', 'no', 'negro', 'L')],
 
-            [None, Torre('v', 'n', 'r', 'R', (1, 1)), None, None, None, None, None, Alfil('v', 'n', 'r', 'B', (1, 7)), None,],
+            [None, Torre('v', 'no', 'negro', 'R'), None, None, None, None, None, Alfil('v', 'no', 'negro', 'B'), None],
 
-            [Peon('v', 'n', 'r', 'P', (2, 0)), Peon('v', 'n', 'r', 'P', (2, 1)), Peon('v', 'n', 'r', 'P', (2, 2)),
-             Peon('v', 'n', 'r', 'P', (2, 3)), Peon('v', 'n', 'r', 'P', (2, 4)), Peon('v', 'n', 'r', 'P', (2, 5)),
-             Peon('v', 'n', 'r', 'P', (2, 6)), Peon('v', 'n', 'r', 'P', (2, 7)), Peon('v', 'n', 'r', 'P', (2, 8))],
+            [Peon('v', 'no', 'negro', 'P'), Peon('v', 'no', 'negro', 'P'), Peon('v', 'no', 'negro', 'P'),
+             Peon('v', 'no', 'negro', 'P'), Peon('v', 'no', 'negro', 'P'), Peon('v', 'no', 'negro', 'P'),
+             Peon('v', 'no', 'negro', 'P'), Peon('v', 'no', 'negro', 'P'), Peon('v', 'no', 'negro', 'P')],
 
             [None, None, None, None, None, None, None, None, None],
             [None, None, None, None, None, None, None, None, None],
             [None, None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None, None],
 
-            [Peon('v', 'n', 'n', 'P', (2, 0)), Peon('v', 'n', 'n', 'P', (2, 1)), Peon('v', 'n', 'n', 'P', (2, 2)),
-             Peon('v', 'n', 'n', 'P', (2, 3)), Peon('v', 'n', 'n', 'P', (2, 4)), Peon('v', 'n', 'n', 'P', (2, 5)),
-             Peon('v', 'n', 'n', 'P', (2, 6)), Peon('v', 'n', 'n', 'P', (2, 7)), Peon('v', 'n', 'n', 'P', (2, 8))],
+            [Peon('v', 'no', 'blanco', 'P'), Peon('v', 'no', 'blanco', 'P'), Peon('v', 'no', 'blanco', 'P'),
+             Peon('v', 'no', 'blanco', 'P'), Peon('v', 'no', 'blanco', 'P'), Peon('v', 'no', 'blanco', 'P'),
+             Peon('v', 'no', 'blanco', 'P'), Peon('v', 'no', 'blanco', 'P'), Peon('v', 'no', 'blanco', 'P')],
 
-            [None, Alfil('v', 'n', 'n', 'B', (1, 7)), None, None, None, None, None, Torre('v', 'n', 'n', 'R', (1, 1)),
-             None, ],
+            [None, Alfil('v', 'no', 'blanco', 'B'), None, None, None, None, None, Torre('v', 'no', 'blanco', 'R'), None],
 
-            [Lancero('v', 'n', 'n', 'L', (0, 1)), Caballo('v', 'n', 'n', 'N', (0, 4)),
-             GeneralPlata('v', 'n', 'n', 'S', (0, 4)),
-             GeneralOro('v', 'n', 'n', 'G', (0, 4)), Rey('v', 'n', 'n', 'K', (0, 5), False),
-             GeneralOro('v', 'n', 'n', 'G', (0, 6)),
-             GeneralPlata('v', 'n', 'n', 'S', (0, 7)), Caballo('v', 'n', 'n', 'N', (0, 8)),
-             Lancero('v', 'n', 'n', 'L', (0, 9))],
-
+            [Lancero('v', 'no', 'blanco', 'L'), Caballo('v', 'no', 'blanco', 'N'), GeneralPlata('v', 'no', 'blanco', 'S'),
+             GeneralOro('v', 'blanco', 'G'), Rey('v', 'blanco', 'K', False), GeneralOro('v', 'blanco', 'G'),
+             GeneralPlata('v', 'no', 'blanco', 'S'), Caballo('v', 'no', 'blanco', 'N'), Lancero('v', 'no', 'blanco', 'L')],
         ]
 
-    def __str__(self):
-        return self.t
-
+        self.tab = numpy.array(self.t)
 
     def imprimir(self):
-        columna = '| | |1| |2| |3| |4| |5| |6| |7| |8| |9|'
-        fila = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
         k = 0
-        print(columna)
-        for i in self.t:
-            print('|'+fila[k]+'| ', end='')
+        print("| | ", end='')
+        for k in range(len(self.columna)):
+            print('|'+self.fila[k]+'| ', end='')
+        print("")
+        k = 0
+        for i in self.tab:
+            print('|'+self.fila[k]+'| ', end='')
             for j in i:
                 if j:
                     print('|'+j.nombre+'| ', end='')
                 else:
                     print('| | ', end='')
-            k =+ 1
+            if k < 8:
+                k += 1
             print('', end='\n')
 
 
-    def verificar_jaque_rojo(self):
-        for i in self.t:
-            for j in i:
-                if j == Rey and j.color == 'r' and j.jaque == True:
-                    return 'rey rojo en jaque'
-
-
     def verificar_jaque_negro(self):
-        for i in self.t:
+        for i in self.tab:
             for j in i:
-                if j == Rey and j.color == 'n' and j.jaque == True:
-                    return 'rey negro en jaque'
+                if j == Rey and j.color == 'negro' and j.jaque == True:
+                    return True
+
+    def verificar_jaque_blanco(self):
+        for i in self.tab:
+            for j in i:
+                if j == Rey and j.color == 'blanco' and j.jaque == True:
+                    return True
+
 
 
 class Jugador:
 
-    def __init__(self, color, piezas):
+    def __init__(self, color):
         self.color = color
 
-    def hacer_jugada(self):
-        pass
+    def hacer_jugada(self, inicial, final):
+        # Si es una pieza entro
+        if tablero.tab[inicial[0]][inicial[1]]:
+            # Verifico que tipo de pieza es para despues analizar el movimiento
+            print(tablero.tab[inicial[0]][inicial[1]])
+            if tablero.tab[inicial[0]][inicial[1]].__class__ is Peon:
+                # Verifico en este caso si solo puede mover un lugar hacia adelante (Suponiendo que no es pieza con promocion)
+                print(final[0])
+                print(inicial[0])
+                print(inicial[1])
+                print(final[1]+1)
+                if final[1] == inicial[1] and final[0] == inicial[0]+1:
+                    print("entra")
+                    #Verificamos que la posicion final este libre y asignamos la nueva posicion
+                    if tablero.tab[final[0]+1][final[1]] == None:
+                        tablero.tab[final[0]][final[1]] = tablero.tab[inicial[0]][inicial[1]]
+                        tablero.tab[inicial[0]][inicial[1]] = None
 
 
 class Pieza:
 
-    def __init__(self, estado, promocion, nombre):
+    def __init__(self, estado,color, nombre):
         self.estado = estado
-        self.promocion = promocion
+        self.color = color
         self.nombre = nombre
 
     def imprimir(self):
@@ -97,13 +108,12 @@ class Pieza:
 
 class Rey(Pieza):
 
-    def __init__(self, estado, promocion, color, nombre, posicion, jaque):
-        super().__init__(estado, promocion, nombre)  # Pongo super() para usar herencia sin self
+    def __init__(self, estado, color, nombre, jaque):
+        super().__init__(estado, color, nombre)  # Pongo super() para usar herencia sin self
 
         self.nombre = nombre
-        self.posicion = posicion
         self.color = color
-        self.color = jaque
+        self.jaque = jaque
 
     def __str__(self):
         return self.nombre
@@ -111,88 +121,103 @@ class Rey(Pieza):
 
 class Lancero(Pieza):
 
-    def __init__(self, estado, promocion, color, nombre, posicion):
-        super().__init__(estado, promocion, nombre)  # Pongo super() para usar herencia sin self
+    def __init__(self, estado, promocion, color, nombre):
+        super().__init__(estado, color, nombre)  # Pongo super() para usar herencia sin self
 
         self.nombre = nombre
-        self.posicion = posicion
         self.color = color
+        self.promocion = promocion
 
 
 class Caballo(Pieza):
 
-    def __init__(self, estado, promocion, color, nombre, posicion):
-        super().__init__(estado, promocion, nombre)  # Pongo super() para usar herencia sin self
+    def __init__(self, estado, promocion, color, nombre):
+        super().__init__(estado, color, nombre)  # Pongo super() para usar herencia sin self
 
         self.nombre = nombre
-        self.posicion = posicion
         self.color = color
-
+        self.promocion = promocion
 
 class GeneralOro(Pieza):
 
-    def __init__(self, estado, promocion, color, nombre, posicion):
-        super().__init__(estado, promocion, nombre)  # Pongo super() para usar herencia sin self
+    def __init__(self, estado, color, nombre):
+        super().__init__(estado, color, nombre)  # Pongo super() para usar herencia sin self
 
         self.nombre = nombre
-        self.posicion = posicion
         self.color = color
 
 
 class GeneralPlata(Pieza):
 
-    def __init__(self, estado, promocion, color, nombre, posicion):
-        super().__init__(estado, promocion, nombre)  # Pongo super() para usar herencia sin self
+    def __init__(self, estado, promocion, color, nombre):
+        super().__init__(estado, color, nombre)  # Pongo super() para usar herencia sin self
 
         self.nombre = nombre
-        self.posicion = posicion
         self.color = color
+        self.promocion = promocion
 
 
 class Alfil(Pieza):
 
-    def __init__(self, estado, promocion, color, nombre, posicion):
-        super().__init__(estado, promocion, nombre)  # Pongo super() para usar herencia sin self
+    def __init__(self, estado, promocion, color, nombre):
+        super().__init__(estado, color, nombre)  # Pongo super() para usar herencia sin self
 
         self.nombre = nombre
-        self.posicion = posicion
         self.color = color
+        self.promocion = promocion
 
 
 class Torre(Pieza):
 
-    def __init__(self, estado, promocion, color, nombre, posicion):
-        super().__init__(estado, promocion, nombre)  # Pongo super() para usar herencia sin self
+    def __init__(self, estado, promocion, color, nombre):
+        super().__init__(estado, color, nombre)  # Pongo super() para usar herencia sin self
 
         self.nombre = nombre
-        self.posicion = posicion
         self.color = color
+        self.promocion = promocion
 
 
 class Peon(Pieza):
 
-    def __init__(self, estado, promocion, color, nombre, posicion):
-        super().__init__(estado, promocion, nombre)  # Pongo super() para usar herencia sin self
+    def __init__(self, estado, promocion, color, nombre):
+        super().__init__(estado, color, nombre)  # Pongo super() para usar herencia sin self
 
         self.nombre = nombre
-        self.posicion = posicion
         self.color = color
+        self.promocion = promocion
 
 
-def devolver_nombre(p):
-    for i in p:
-        print(i.nombre)
-
-
-jugador_negro = Jugador('rojo', 'negro')
-jugador_rojo = Jugador('negro', 'negro')
+j1 = Jugador('negro')
+j2 = Jugador('blanco')
 tablero = Tablero()
 
 while tablero.juego != 'termiando':
-    if tablero.verificar_jaque_negro():
-        jugador_negro.hacer_jugada()
     tablero.imprimir()
-    if tablero.verificar_jaque_rojo():
-        jugador_rojo.hacer_jugada()
+    negro_inicial = [None,'']
+    negro_final = [None, '']
+    blanco_inicial = [None, '']
+    blanco_final = [None, '']
+    if not tablero.verificar_jaque_negro():
+        entrada = input("Jugador negro, ingrese posicion x e y de pieza a mover (Separados por un espacio):")
+        x, y = (int(item) for item in entrada.split())
+        negro_inicial = (x, y)
+        entrada = input("Jugador negro, ingrese posicion x e y de destino de pieza (Separados por un espacio): ")
+        x, y = (int(item) for item in entrada.split())
+        negro_final = (x, y)
+        try:
+            j1.hacer_jugada(negro_inicial, negro_final)
+        except ValueError:
+            print("La jugada es incorrecta")
     tablero.imprimir()
+    if not tablero.verificar_jaque_blanco():
+        entrada = input("Jugador blanco, ingrese posicion x e y de pieza a mover (Separados por un espacio): ")
+        x, y = (int(item) for item in entrada.split())
+        blanco_inicial = (x, y)
+        entrada = input("Jugador negro, ingrese posicion x e y de destino de pieza (Separados por un espacio): ")
+        x, y = (int(item) for item in entrada.split())
+        blanco_final = (x, y)
+        try:
+            j2.hacer_jugada(blanco_inicial, blanco_final)
+        except ValueError:
+            print("La jugada es incorrecta")
 

7fe831862388224297f09d39cf67fbeced168df4
Javier Valdivieso <javier.valdivieso@possumus.tech>
2019-11-14 23:33:02 -0300

    Se agrega movimeintos de promocion a peon. Se corrigen turnos de jugadores y se agregan flujos en caso de errores. Se agrega verificacion de piezas muertas a funcion imprimir.

--- shogy.py
+++ shogy.py
@@ -49,7 +49,10 @@ class Tablero:
             print('|'+self.fila[k]+'| ', end='')
             for j in i:
                 if j:
-                    print('|'+j.nombre+'| ', end='')
+                    if j.estado == 'v':
+                        print('|'+j.nombre+'| ', end='')
+                    else:
+                        print('| | ', end='')
                 else:
                     print('| | ', end='')
             if k < 8:
@@ -76,28 +79,120 @@ class Jugador:
     def __init__(self, color):
         self.color = color
 
-    def hacer_jugada(self, inicial, final):
+    def hacer_jugada(self, color, inicial, final):
         # Si es una pieza entro
+        print("adentro")
         if tablero.tab[inicial[0]][inicial[1]]:
             # Verifico que tipo de pieza es para despues analizar el movimiento
-            print(tablero.tab[inicial[0]][inicial[1]])
-            if tablero.tab[inicial[0]][inicial[1]].__class__ is Peon:
-                # Verifico en este caso si solo puede mover un lugar hacia adelante (Suponiendo que no es pieza con promocion)
-                print(final[0])
-                print(inicial[0])
-                print(inicial[1])
-                print(final[1]+1)
-                if final[1] == inicial[1] and final[0] == inicial[0]+1:
-                    print("entra")
-                    #Verificamos que la posicion final este libre y asignamos la nueva posicion
-                    if tablero.tab[final[0]+1][final[1]] == None:
-                        tablero.tab[final[0]][final[1]] = tablero.tab[inicial[0]][inicial[1]]
-                        tablero.tab[inicial[0]][inicial[1]] = None
+            if tablero.tab[inicial[0]][inicial[1]].color == color:
+                if tablero.tab[inicial[0]][inicial[1]].__class__ is Peon:
+                # Verifico si la pieza tiene promocion
+                    if tablero.tab[inicial[0]][inicial[1]].promocion == 'no':
+                        # El peon no está en estado de promocion por lo que solo puede mover un lugar adelante
+                            if tablero.tab[inicial[0]][inicial[1]].color == 'negro':
+                                # Si es una pieza del jugador negro solo puede mover hacia abajo
+                                if final[1] == inicial[1] and final[0] == inicial[0]+1:
+                                    # Si el destino de la ficha esta libre, ponemos el peon en ese lugar
+                                    if tablero.tab[final[0]][final[1]] is None:
+                                        tablero.tab[final[0]][final[1]] = tablero.tab[inicial[0]][inicial[1]]
+                                        tablero.tab[inicial[0]][inicial[1]] = None
+                                        print("Pieza movida")
+                                        return True
+                                    else:
+                                        # Si el destino está ocupado por una pieza del otro jugador se debe poner la pieza en estado muerta y cambiar el color, sino se cancela la jugada
+                                        if tablero.tab[final[0]][final[1]].color == 'blanco':
+                                            tablero.tab[final[0]][final[1]].estado = 'm'
+                                            tablero.tab[final[0]][final[1]].estado = 'negro'
+                                            tablero.tab[final[0]][final[1]] = tablero.tab[inicial[0]][inicial[1]]
+                                            print("Pieza comida")
+                                            return True
+                                        else:
+                                            # Cancelar jugada porque no puedo comer mi propia pieza
+                                            return False
+                                else:
+                                    return False
+                            else:
+                                # Si es una pieza del jugador blanco solo puede mover hacia arriba
+                                if final[1] == inicial[1] and final[0] == inicial[0] -1:
+                                    # Si el destino de la ficha esta libre, ponemos el peon en ese lugar
+                                    if tablero.tab[final[0]][final[1]] is None:
+                                        tablero.tab[final[0]][final[1]] = tablero.tab[inicial[0]][inicial[1]]
+                                        tablero.tab[inicial[0]][inicial[1]] = None
+                                        print("Pieza movida")
+                                        return True
+                                    else:
+                                        # Si el destino está ocupado por una pieza del otro jugador se debe poner la pieza en estado muerta y cambiar el color, sino se cancela la jugada
+                                        if tablero.tab[final[0]][final[1]].color == 'negro':
+                                            tablero.tab[final[0]][final[1]].estado = 'm'
+                                            tablero.tab[final[0]][final[1]].estado = 'blanco'
+                                            tablero.tab[final[0]][final[1]] = tablero.tab[inicial[0]][inicial[1]]
+                                            print("Pieza comida")
+                                            return True
+                                        else:
+                                            # Cancelar jugada porque no puedo comer mi propia pieza
+                                            return False
+                                else:
+                                    return False
+                    else:
+                        # El peon está en estado de promocion y puede mover como general de oro
+                        if tablero.tab[inicial[0]][inicial[1]].color == 'negro':
+                        # Si es una pieza del jugador negro puede mover hacia abajo, los costados y solo derecho hacia arriba
+                            if final[0] == inicial[0]+1 and final[1] == inicial[1] or final[0] == inicial[0]-1 and final[1] == inicial[1] \
+                            or final[0] == inicial[0] and final[1] == inicial[1]+1 or final[0] == inicial[0] and final[1] == inicial[1]-1 \
+                            or final[0] == inicial[0] and final[1] == inicial[1]+1 or final[0] == inicial[0]+1 and final[1] == inicial[1]-1:
+                            # Si el destino de la ficha esta libre, ponemos el peon en ese lugar
+                                if tablero.tab[final[0]][final[1]] is None:
+                                    tablero.tab[final[0]][final[1]] = tablero.tab[inicial[0]][inicial[1]]
+                                    tablero.tab[inicial[0]][inicial[1]] = None
+                                    print("Pieza movida")
+                                    return True
+                                else:
+                                # Si el destino está ocupado por una pieza del otro jugador se debe poner la pieza en estado muerta y cambiar el color, sino se cancela la jugada
+                                    if tablero.tab[final[0]][final[1]].color == 'blanco':
+                                        tablero.tab[final[0]][final[1]].estado = 'm'
+                                        tablero.tab[final[0]][final[1]].estado = 'negro'
+                                        tablero.tab[final[0]][final[1]] = tablero.tab[inicial[0]][inicial[1]]
+                                        print("Pieza comida")
+                                        return True
+                                    else:
+                                    # Cancelar jugada porque no puedo comer mi propia pieza
+                                        return False
+                            else:
+                                return False
+                        else:
+                        # Si es una pieza del jugador blanco puede mover hacia arriba, los costados y solo derecho hacia abajo
+                        if final[0] == inicial[0] + 1 and final[1] == inicial[1] or final[0] == inicial[0] - 1 and final[1] == inicial[1] \
+                        or final[0] == inicial[0] and final[1] == inicial[1] + 1 or final[0] == inicial[0] and final[1] == inicial[1] - 1 \
+                        or final[0] == inicial[0]-1 and final[1] == inicial[1] + 1 or final[0] == inicial[0]-1 and final[1] == inicial[1]+1:
+                                # Si el destino de la ficha esta libre, ponemos el peon en ese lugar
+                                if tablero.tab[final[0]][final[1]] is None:
+                                    tablero.tab[final[0]][final[1]] = tablero.tab[inicial[0]][inicial[1]]
+                                    tablero.tab[inicial[0]][inicial[1]] = None
+                                    print("Pieza movida")
+                                    return True
+                                else:
+                                    # Si el destino está ocupado por una pieza del otro jugador se debe poner la pieza en estado muerta y cambiar el color, sino se cancela la jugada
+                                    if tablero.tab[final[0]][final[1]].color == 'negro':
+                                        tablero.tab[final[0]][final[1]].estado = 'm'
+                                        tablero.tab[final[0]][final[1]].estado = 'blanco'
+                                        tablero.tab[final[0]][final[1]] = tablero.tab[inicial[0]][inicial[1]]
+                                        print("Pieza comida")
+                                        return True
+                                    else:
+                                        # Cancelar jugada porque no puedo comer mi propia pieza
+                                        return False
+                elif tablero.tab[inicial[0]][inicial[1]].__class__ is Lancero:
+                    pass
+            else:
+                print("Pieza de adversario")
+                return False
+        else:
+            return False
 
 
 class Pieza:
 
-    def __init__(self, estado,color, nombre):
+    def __init__(self, estado, color, nombre):
         self.estado = estado
         self.color = color
         self.nombre = nombre
@@ -111,6 +206,7 @@ class Rey(Pieza):
     def __init__(self, estado, color, nombre, jaque):
         super().__init__(estado, color, nombre)  # Pongo super() para usar herencia sin self
 
+        self.estado = estado
         self.nombre = nombre
         self.color = color
         self.jaque = jaque
@@ -124,6 +220,7 @@ class Lancero(Pieza):
     def __init__(self, estado, promocion, color, nombre):
         super().__init__(estado, color, nombre)  # Pongo super() para usar herencia sin self
 
+        self.estado = estado
         self.nombre = nombre
         self.color = color
         self.promocion = promocion
@@ -134,6 +231,7 @@ class Caballo(Pieza):
     def __init__(self, estado, promocion, color, nombre):
         super().__init__(estado, color, nombre)  # Pongo super() para usar herencia sin self
 
+        self.estado = estado
         self.nombre = nombre
         self.color = color
         self.promocion = promocion
@@ -143,6 +241,7 @@ class GeneralOro(Pieza):
     def __init__(self, estado, color, nombre):
         super().__init__(estado, color, nombre)  # Pongo super() para usar herencia sin self
 
+        self.estado = estado
         self.nombre = nombre
         self.color = color
 
@@ -152,6 +251,7 @@ class GeneralPlata(Pieza):
     def __init__(self, estado, promocion, color, nombre):
         super().__init__(estado, color, nombre)  # Pongo super() para usar herencia sin self
 
+        self.estado = estado
         self.nombre = nombre
         self.color = color
         self.promocion = promocion
@@ -162,6 +262,7 @@ class Alfil(Pieza):
     def __init__(self, estado, promocion, color, nombre):
         super().__init__(estado, color, nombre)  # Pongo super() para usar herencia sin self
 
+        self.estado = estado
         self.nombre = nombre
         self.color = color
         self.promocion = promocion
@@ -172,6 +273,7 @@ class Torre(Pieza):
     def __init__(self, estado, promocion, color, nombre):
         super().__init__(estado, color, nombre)  # Pongo super() para usar herencia sin self
 
+        self.estado = estado
         self.nombre = nombre
         self.color = color
         self.promocion = promocion
@@ -182,6 +284,7 @@ class Peon(Pieza):
     def __init__(self, estado, promocion, color, nombre):
         super().__init__(estado, color, nombre)  # Pongo super() para usar herencia sin self
 
+        self.estado = estado
         self.nombre = nombre
         self.color = color
         self.promocion = promocion
@@ -197,27 +300,61 @@ while tablero.juego != 'termiando':
     negro_final = [None, '']
     blanco_inicial = [None, '']
     blanco_final = [None, '']
-    if not tablero.verificar_jaque_negro():
+    turno1 = True
+    turno2 = True
+    while turno1 is True:
         entrada = input("Jugador negro, ingrese posicion x e y de pieza a mover (Separados por un espacio):")
-        x, y = (int(item) for item in entrada.split())
-        negro_inicial = (x, y)
-        entrada = input("Jugador negro, ingrese posicion x e y de destino de pieza (Separados por un espacio): ")
-        x, y = (int(item) for item in entrada.split())
-        negro_final = (x, y)
         try:
-            j1.hacer_jugada(negro_inicial, negro_final)
-        except ValueError:
-            print("La jugada es incorrecta")
-    tablero.imprimir()
-    if not tablero.verificar_jaque_blanco():
-        entrada = input("Jugador blanco, ingrese posicion x e y de pieza a mover (Separados por un espacio): ")
-        x, y = (int(item) for item in entrada.split())
-        blanco_inicial = (x, y)
+            x, y = (int(item) for item in entrada.split())
+        except:
+            print("Valor incorrecto")
+            break
+        if 9 > x >= 0 and 9 > y >= 0:
+            negro_inicial = (x, y)
+        else:
+            print("Valor incorrecto, solo se permiten enteros positivos entre 0 y 9")
+            break
         entrada = input("Jugador negro, ingrese posicion x e y de destino de pieza (Separados por un espacio): ")
-        x, y = (int(item) for item in entrada.split())
-        blanco_final = (x, y)
         try:
-            j2.hacer_jugada(blanco_inicial, blanco_final)
-        except ValueError:
-            print("La jugada es incorrecta")
+            x, y = (int(item) for item in entrada.split())
+        except:
+            print("Valor incorrecto")
+        if 9 > x >= 0 and 9 > y >= 0:
+            negro_final = (x, y)
+        else:
+            print("Valor incorrecto, solo se permiten enteros positivos entre 0 y 9")
+            break
+        r = j1.hacer_jugada(j1.color, negro_inicial, negro_final)
+        if r is True:
+            turno1 = False
 
+    tablero.imprimir()
+
+    while turno2 is True:
+        entrada = input("Jugador blanco, ingrese posicion x e y de pieza a mover (Separados por un espacio):")
+        try:
+            x, y = (int(item) for item in entrada.split())
+        except:
+            print("Valor incorrecto")
+            break
+        if 9 > x >= 0 and 9 > y >= 0:
+            blanco_inicial = (x, y)
+        else:
+            print("Valor incorrecto, solo se permiten enteros positivos entre 0 y 9")
+            break
+        entrada = input("Jugador blanco, ingrese posicion x e y de destino de pieza (Separados por un espacio): ")
+        try:
+            x, y = (int(item) for item in entrada.split())
+        except:
+            print("Valor incorrecto")
+        if 9 > x >= 0 and 9 > y >= 0:
+            blanco_final = (x, y)
+        else:
+            print("Valor incorrecto, solo se permiten enteros positivos entre 0 y 9")
+            break
+        r = j2.hacer_jugada(j2.color, blanco_inicial, blanco_final)
+        if r is True:
+            turno2 = False
+
+    turno1 = True
+    turno2 = True
